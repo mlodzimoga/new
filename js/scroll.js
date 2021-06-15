5d50cd196e490152dc9ba9cfc1a3f96c3b0a2d4c
jakubLabedowicz <jalabedowicz@gmail.com>
2021-06-15 16:29:55 +0200

    edit

--- js/scroll.js
+++ js/scroll.js
@@ -8,10 +8,6 @@ function myFunction() {
         document.getElementById("nav").style.boxShadow = "12px 12px 96px var(--cardComponentshadowColor)";
 
         document.getElementById("nav-title").style.color = "var(--cardHeaderColor)";
-        // document.querySelector("#b1").style.backgroundColor = "#ffffff";
-        // document.querySelector("#b2").style.backgroundColor = "#ffffff";
-        // document.querySelector("#b3").style.backgroundColor = "#ffffff";
-        // document.querySelector("#b4").style.backgroundColor = "#ffffff";
 
         document.querySelector("#b1").style.color = "var(--cardHeaderColor)";
         document.querySelector("#b2").style.color = "var(--cardHeaderColor)";
@@ -24,10 +20,6 @@ function myFunction() {
         document.getElementById("nav").style.boxShadow = "none";
 
         document.getElementById("nav-title").style.color = "#ffffff";
-        document.querySelector("#b1").style.backgroundColor = "#ffffff00";
-        document.querySelector("#b2").style.backgroundColor = "#ffffff00";
-        document.querySelector("#b3").style.backgroundColor = "#ffffff00";
-        document.querySelector("#b4").style.backgroundColor = "#ffffff00";
 
         document.querySelector("#b1").style.color = "#ffffff";
         document.querySelector("#b2").style.color = "#ffffff";
